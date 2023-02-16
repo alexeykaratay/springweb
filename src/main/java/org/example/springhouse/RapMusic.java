77f4bb4ec8b0890269685077a8b0ae85d76a4dc2
Alexey Karatay <alexeykaratay@yandex.ru>
2023-02-17 01:15:11 +0300

    rename

--- src/main/java/org/example/springhouse/RapMusic.java
+++ src/main/java/org/example/springhouse/RapMusic.java
@@ -1,20 +1,12 @@
 package org.example.springhouse;
 
 import org.springframework.stereotype.Component;
-import java.util.ArrayList;
-import java.util.List;
 
 @Component("musicRap")
 public class RapMusic implements Music {
-    private List<String> songs = new ArrayList<>();
-    {
-        songs.add("50 Cent- Candy Shop");
-        songs.add("Eminem- Stan");
-        songs.add("2Pak- I Get Around");
-    }
 
     @Override
-    public List<String> getSong() {
-        return songs;
+    public String getSong() {
+        return "50 Cent- Candy Shop";
     }
 }
