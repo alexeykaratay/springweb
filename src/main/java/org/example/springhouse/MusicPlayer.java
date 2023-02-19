5578059abfccf233997ccaa9564b262204c579c1
Alexey Karatay <alexeykaratay@yandex.ru>
2023-02-19 21:45:57 +0300

    MusicPlayer

--- src/main/java/org/example/springhouse/MusicPlayer.java
+++ src/main/java/org/example/springhouse/MusicPlayer.java
@@ -2,7 +2,6 @@ package org.example.springhouse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
